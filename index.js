10fe490f933dc7042cac11b8191fa8fbc974f538
stephendwalley <76471970+stephendwalley@users.noreply.github.com>
2021-02-07 22:54:06 +1100

    Added some simple button styles and basic grid structure for html/css layout

---
+++ index.js
@@ -0,0 +1,19 @@
+// Base Calculation Functions
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+// function operate(a, b, operator) {}
