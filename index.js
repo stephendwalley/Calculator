dfa78936ee6edeef88157bf15f5f922d5fe654db
stephendwalley <76471970+stephendwalley@users.noreply.github.com>
2021-02-10 20:38:18 +1100

    Ability to store one number as a variabe

--- index.js
+++ index.js
@@ -16,36 +16,57 @@ function divide(a, b) {
 	return a / b;
 }
 
-// Display functionality
-let display = document.querySelector("#display");
+//Button Functionality
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
 	button.addEventListener("click", () => {
-		modifyDisplay(button.textContent);
-		console.log(button.id);
+		if (button.id === "btnEqual") {
+			operate();
+		} else {
+			modifyDisplay(button.textContent);
+			console.log(button.id);
+		}
 	});
 });
+
+// Display functionality
+let display = document.querySelector("#display");
 
 function modifyDisplay(input) {
 	if (display.textContent === "0.00") {
 		console.log("it is 0!");
 		display.textContent = input;
+		storeNumber(input);
 	} else {
 		console.log("it is not 0!");
 		display.textContent += input;
+		storeNumber(input);
 	}
 }
 
 // Operation of calculator to call functions when equal is pressed
+let firstNumber = 0;
+let secondNumber = 0;
+let choiceOperator = "";
+
+function storeNumber(input) {
+	if (Number.isInteger(parseFloat(input))) {
+		firstNumber += input;
+		console.log("It is a number!");
+	} else {
+		console.log("It is not a number!");
+	}
+}
+
 function operate(a, b, operator) {
-	if (operator === add) {
+	if (operator === "add") {
 		add(a, b);
-	} else if (operator === subtract) {
+	} else if (operator === "subtract") {
 		subtract(a, b);
-	} else if (operator === multiply) {
+	} else if (operator === "multiply") {
 		multiply(a, b);
-	} else if (operator === divide) {
+	} else if (operator === "divide") {
 		divide(a, b);
 	}
 }
