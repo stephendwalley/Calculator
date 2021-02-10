1b5c941fb6d8d45a0e230bf6f9f02f15455af57f
stephendwalley <76471970+stephendwalley@users.noreply.github.com>
2021-02-10 20:10:48 +1100

    Working display

--- index.js
+++ index.js
@@ -16,12 +16,26 @@ function divide(a, b) {
 	return a / b;
 }
 
+// Display functionality
+let display = document.querySelector("#display");
+
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
 	button.addEventListener("click", () => {
+		modifyDisplay(button.textContent);
 		console.log(button.id);
 	});
 });
+
+function modifyDisplay(input) {
+	if (display.textContent === "0.00") {
+		console.log("it is 0!");
+		display.textContent = input;
+	} else {
+		console.log("it is not 0!");
+		display.textContent += input;
+	}
+}
 
 // Operation of calculator to call functions when equal is pressed
 function operate(a, b, operator) {
@@ -35,12 +49,3 @@ function operate(a, b, operator) {
 		divide(a, b);
 	}
 }
-
-// Display functionality
-
-function modifyDisplay(inputText) {
-	let displayText = document.querySelector("#display");
-	displayText.childNode.textContent = inputText;
-	return;
-}
-
