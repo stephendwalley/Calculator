68ae763fad6f29459ef03fa58dbbef92bc7fb521
stephendwalley <76471970+stephendwalley@users.noreply.github.com>
2021-02-12 14:41:45 +1100

    Working array adjustment to operate the calculator

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Base Calculation Functions
 
 function add(a, b) {
-	return a + b;
+	console.log(a +b);
 }
 
 function subtract(a, b) {
-	return a - b;
+	console.log(a - b);
 }
 
 function multiply(a, b) {
@@ -22,9 +22,10 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
 	button.addEventListener("click", () => {
 		if (button.id === "btnEqual") {
-			operate();
+			saveInput();
 		} else {
 			modifyDisplay(button.textContent);
+			inputArray.push(button.textContent);
 			console.log(button.id);
 		}
 	});
@@ -37,36 +38,47 @@ function modifyDisplay(input) {
 	if (display.textContent === "0.00") {
 		console.log("it is 0!");
 		display.textContent = input;
-		storeNumber(input);
+		// storeNumber(input);
 	} else {
 		console.log("it is not 0!");
 		display.textContent += input;
-		storeNumber(input);
+		// storeNumber(input);
 	}
 }
 
-// Operation of calculator to call functions when equal is pressed
-let firstNumber = 0;
-let secondNumber = 0;
-let choiceOperator = "";
+// Using an array to save all the inputed values from the calculator. Then split the array into specific parts based on where the operator comes. When equals is pressed array is cleared and replaced with the solution.
 
-function storeNumber(input) {
-	if (Number.isInteger(parseFloat(input))) {
-		firstNumber += input;
-		console.log("It is a number!");
-	} else {
-		console.log("It is not a number!");
-	}
+let inputArray = [];
+
+function saveInput() {
+	let numberString = inputArray
+		.toString()
+		.replace("+", " + ")
+		.replace("−", " − ")
+		.replace("×", " × ")
+		.replace("÷", " ÷ ")
+		.replaceAll(",", "");
+	let adjustedString = numberString.split(" ");
+	console.log(adjustedString);
+	operate.apply(this, adjustedString);
 }
 
-function operate(a, b, operator) {
-	if (operator === "add") {
+function operate(a, operator, b) {
+	if (operator === "+") {
 		add(a, b);
-	} else if (operator === "subtract") {
+		console.log("It is adding!");
+	} else if (operator === "−") {
 		subtract(a, b);
-	} else if (operator === "multiply") {
+		console.log("It is subtracting!");
+	} else if (operator === "×") {
 		multiply(a, b);
-	} else if (operator === "divide") {
+		console.log("It is multiplying");
+	} else if (operator === "÷") {
 		divide(a, b);
+		console.log("It is dividing!");
+	} else {
+		console.log("error");
 	}
 }
+
+// OPERATORS − × ÷ +
